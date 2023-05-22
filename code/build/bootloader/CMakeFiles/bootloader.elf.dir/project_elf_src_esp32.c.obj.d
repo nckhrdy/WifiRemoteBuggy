8482cd089bbd447fc24a5b2e9db23446cbaa378d
Nicholas Hardy <91441308+nckhrdy@users.noreply.github.com>
2023-05-22 19:32:01 -0400

    Delete code/build directory

--- code/build/bootloader/CMakeFiles/bootloader.elf.dir/project_elf_src_esp32.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/bootloader.elf.dir/project_elf_src_esp32.c.obj: \
- /Users/nickhardy/Documents/GitHub/Team1-Boujaoude-Deokar-Hijazi-Hardy/quest-6/code/build/bootloader/project_elf_src_esp32.c
--- code/build/bootloader/esp-idf/soc/CMakeFiles/__idf_soc.dir/esp32/sdm_periph.c.obj.d
+++
@@ -1,6 +0,0 @@
-esp-idf/soc/CMakeFiles/__idf_soc.dir/esp32/sdm_periph.c.obj: \
- /Users/nickhardy/esp/esp-idf/components/soc/esp32/sdm_periph.c \
- /Users/nickhardy/esp/esp-idf/components/soc/include/soc/sdm_periph.h \
- /Users/nickhardy/esp/esp-idf/components/soc/esp32/include/soc/soc_caps.h \
- /Users/nickhardy/Documents/GitHub/Team1-Boujaoude-Deokar-Hijazi-Hardy/quest-6/code/build/bootloader/config/sdkconfig.h \
- /Users/nickhardy/esp/esp-idf/components/soc/esp32/include/soc/gpio_sig_map.h
